8ccfb4807f8bf2b6a5fe3790ef212b2ad60c35af
Guilherme Abreu <abreugui@hotmail.com>
2023-07-10 20:16:57 -0300

    Arrumando o README.md e completando a operação Depósito

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ Fui contratado por um banco para desenvolver seu novo sistema, e o mesmo utiliza
 | Operação | Requisitos |
 | --- | --- |
 | Depósito | Somente possível depositar valores positivos, para a primeira versão terá somente 1 usuário, e todos depósitos devem ser armazenados em uma variável |
-| Saque | 2. O máximo de 3 saques diários com limite máximo de R$500 por saque. Caso o usuário não tenha saldo em conta, deve avisar. Todo saque deve ser armazenado em uma variável para o extrato |
+| Saque | O máximo de 3 saques diários com limite máximo de R$500 por saque. Caso o usuário não tenha saldo em conta, deve avisar. Todo saque deve ser armazenado em uma variável para o extrato |
 | Extrato | Deve armazenar todos os depósitos e saques da conta. No fim da listagem deve exibir o saldo da conta. Se não houver movimentações deve exibir: "Não foram realizadas movimentações" |
--- SistemaBancario.py
+++ SistemaBancario.py
@@ -2,33 +2,45 @@
 
 menu = None
 texto = '''
-Olá, selecione uma das uma das operações abaixo:
     [1] Depósito
     [2] Saque
     [3] Extrato
     [4] Sair
 '''
 saldo = 0
-extrato = ""
+extrato = {'deposito':[], 'saque':[]}
 limite = 500
 numero_saques_dia = 0
 LIMITE_SAQUES = 3
 
-print(texto)
+print("Olá, Bem Vindo ao Banco X!","Selecione uma das uma das operações abaixo:",texto, sep="\n")
 
 while True:
 
-    menu = int(input("Selecione:"))
+    menu = str(input("Selecione:"))
     
-    if menu == 1:
-        print("Deposito")
+    if menu == "1":
+        valor = int(input("Digite o valor a ser depositado:"))
+        if valor<=0:
+            print("Valor de depósito inválido!")
+        else:
+            saldo += valor
+            extrato['deposito'].append(valor)
+            print(f"Depósito de R${valor:0.2f} realizado!", end="\n\n")
     
-    elif menu == 2:
-        print("Saque")
+    elif menu == "2":
+        
+        
+        if saldo == 0 | valor > saldo:
+            print("Saldo insuficiente para a operação", f"Seu saldo é {saldo}", end="\n\n")
 
-    elif menu == 3:
-        print("Extrato")
+    elif menu == "3":
+        print(extrato)
 
-    elif menu == 4:
-        print("Saiu")
+    elif menu == "4":
+        print("Saindo. Tenha um bom dia!")
         break
+    else:
+        print("Operação inválida")
+
+    print("Selecione uma das uma das operações abaixo:",texto, sep="\n")
